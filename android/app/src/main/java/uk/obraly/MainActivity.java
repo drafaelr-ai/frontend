7d9c15a3ccbe32bf550c7198a6d3a9375f477707
drafaelr-ai <drafaelr@gmail.com>
2025-11-04 11:01:41 -0300

    ip

---
+++ android/app/src/main/java/uk/obraly/MainActivity.java
@@ -0,0 +1,5 @@
+package uk.obraly;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'uk.obraly',
+  appName: 'Obraly',
+  webDir: 'build'
+};
+
+export default config;
